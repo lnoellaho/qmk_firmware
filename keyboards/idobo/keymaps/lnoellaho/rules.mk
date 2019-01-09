91c412ce13c06c0404e0f3d5377b9ea69d5658aa
lnoellaho <lnoellaho@gmail.com>
2019-01-09 00:47:25 -0800

    backlight enabled + mapped

--- keyboards/idobo/keymaps/lnoellaho/rules.mk
+++ keyboards/idobo/keymaps/lnoellaho/rules.mk
@@ -14,3 +14,4 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
+BACKLIGHT_ENABLE = yes       # Enable keyboard backlight
